98a29cf040263bff4af0a65fb20042caad0a646b
vboxuser <stock2304@hotmail.com>
2024-05-03 17:00:01 +0200

    commit front1

--- components/Trends.js
+++ components/Trends.js
@@ -10,7 +10,7 @@ function Trends() {
   const [trendsData, setTrendsData] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:3000/tweets/trends/${user.token}`)
+    fetch(`http://34.150.214.105:3000/tweets/trends/${user.token}`)
       .then(response => response.json())
       .then(data => {
         data.result && setTrendsData(data.trends);
